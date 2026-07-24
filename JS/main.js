4cb19523a2932c13a6d9bad01298e0b193a4ce02
100Sp4rk100 <100sp4rk100mod@gmail.com>
2026-07-24 16:19:24 +0200

    We can dowload and remove files of internal

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,8 @@ const flagSize = 1;
 var calculator = new Numworks();
 var is_connected = false;
 
+let storage = {};
+
 function calculator_connected(){
     console.log("Connected");
     is_connected = true;
@@ -20,20 +22,13 @@ function getPadingForBufferSize(bufferSize){
 
 async function getInternal(){
     if(is_connected){
-        //clear view
-        let files_list = document.getElementById("internal_files_list");
-
-        while(files_list.firstChild) { 
-            files_list.removeChild(files_list.firstChild); 
-        }
-
         //read storage
         let pinfo = await calculator.getPlatformInfo();
         const startFlash = pinfo.external.flashStart + 327680;
         const endFlash = pinfo.external.flashEnd;
         const flashSize = endFlash - startFlash;
 
-        console.log("Reading storage")
+        console.log("Reading storage");
 
         let blobStorage = await calculator.__retrieveStorage(startFlash, flashSize, pinfo.version);
         
@@ -42,7 +37,7 @@ async function getInternal(){
         const storageArrayBuffer = await blobStorage.arrayBuffer();
         const dataView = new DataView(storageArrayBuffer);
 
-        let storage = {};
+        storage = {};
 
         let adress = startFlash;
         const minHeaderSize = uint32Size * 2 + nameSize + flagSize;
@@ -76,9 +71,106 @@ async function getInternal(){
             let fileName = new TextDecoder().decode(nameBytes).replace(/\0/g, '');
 
             console.log("Finding file : " + fileName);
-            addingInternalFile(fileName);
+            
+            let contentOffset = view_pos + uint32Size * 2 + nameSize + 4;
+            let actualSize = current_buffer_size;
+
+            const firstByte = dataView.getUint8(contentOffset);
+            if (firstByte === 0x01) {
+                contentOffset += 1;
+                actualSize -= 1;
+            }
+
+            if (actualSize > 0) {
+                const lastByte = dataView.getUint8(contentOffset + actualSize - 1);
+                if (lastByte === 0x00 || lastByte === 0xFF) {
+                    actualSize -= 1;
+                }
+            }
+
+            const fileBuffer = storageArrayBuffer.slice(contentOffset, contentOffset + actualSize);
+
+            storage[fileName] = new Blob([fileBuffer], { type: "text/plain" });
 
             adress += (uint32Size * 2) + nameSize + 4 + current_buffer_size + getPadingForBufferSize(current_buffer_size);
+        }
+
+        reloadInternalFileView();
+
+    }else{
+        alert("You need to connect your calculator !");
+
+        calculator.detect(function() {
+            calculator_connected();
+        }, function(error) {
+            alert("Error : " + error);
+        });
+    }
+}
+
+async function uploadInternal(){
+    if(is_connected){
+        if (Object.keys(storage).length == 0){
+            alert("You need to load your files first !");
+            return;
+        }
+
+        // get numworks infos
+        let pinfo = await calculator.getPlatformInfo();
+        const startFlash = pinfo.external.flashStart + 327680;
+        const endFlash = pinfo.external.flashEnd;
+        const flashSize = endFlash - startFlash;
+
+        //make buffer
+        console.log("Create buffer storage");
+
+        const combinedBuffer = new Uint8Array(flashSize);
+        combinedBuffer.fill(0xFF);
+
+        const keys = Object.keys(storage);
+        const encoder = new TextEncoder();
+
+        let position = 0;
+
+        for (const name of keys) {
+            const blob = storage[name];
+            const buffer_size = (uint32Size * 2) + nameSize + 4;
+            const buffer = new Uint8Array(buffer_size);
+            buffer.fill(0xFF);
+
+            const nameOffset = uint32Size * 2;
+            buffer.subarray(nameOffset, nameOffset + nameSize).fill(0x00);
+
+            const view = new DataView(buffer.buffer);
+
+            view.setUint32(0, MAGIC, true);
+
+            view.setUint32(uint32Size, blob.size, true);
+
+            const encodedName = encoder.encode(name);
+            buffer.set(encodedName.subarray(0, nameSize-1), uint32Size * 2);
+
+            const arrayBuffer = await blob.arrayBuffer();
+            const fileContent = new Uint8Array(arrayBuffer);
+
+            combinedBuffer.set(buffer, position);
+            combinedBuffer.set(fileContent, position + buffer_size);
+
+            const padding = getPadingForBufferSize(blob.size);
+            position += buffer_size + blob.size + padding;
+
+        };
+
+        //upload storage
+        console.log("Upload buffer storage");
+
+        try {
+            await calculator.__flashStorage(startFlash, combinedBuffer.buffer, pinfo.version);
+            console.log("Files installed successfully");
+            alert("Files successfully synchronized !");
+        } catch (err) {
+            console.error("Flashing error:", err);
+            alert("Error during synchronization: " + err.message);
         }
 
     }else{
@@ -100,11 +192,26 @@ function addingInternalFile(name){
 
     const download_btn = document.createElement("button");
     download_btn.innerHTML = "Download";
-    download_btn.addEventListener("click", function(){}); //dowload file
+    download_btn.addEventListener("click", function(){
+        const blob = storage[name];
+        if (!blob){
+            return;
+        }
+        
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = name;
+        a.click();
+        URL.revokeObjectURL(url);
+    }); //dowload file
 
     const delete_btn = document.createElement("button");
     delete_btn.innerHTML = "Delete";
-    delete_btn.addEventListener("click", function(){}); // delete file
+    delete_btn.addEventListener("click", function(){
+        delete storage[name];
+        reloadInternalFileView();
+    }); // delete file
 
     container.appendChild(download_btn);
     container.appendChild(delete_btn);
@@ -112,6 +219,23 @@ function addingInternalFile(name){
     files_list.appendChild(container);
 }
 
+function reloadInternalFileView(){
+    //clear view
+    let files_list = document.getElementById("internal_files_list");
+
+    while(files_list.firstChild) { 
+        files_list.removeChild(files_list.firstChild); 
+    }
+
+    // fill view
+    const keys = Object.keys(storage);
+
+    keys.forEach(function(name, i) {
+        addingInternalFile(name);
+    });
+}
+
 calculator.autoConnect(calculator_connected);
 
 document.getElementById("load_internal_files").addEventListener("click", getInternal);
+document.getElementById("sync_internal_files").addEventListener("click", uploadInternal);
